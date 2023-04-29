e72ac65afcb19f8fe3b5d956378913b182ddfd96
Yuki Oshima <39944763+os1ma@users.noreply.github.com>
2023-04-29 12:53:55 +0900

    Use SQLContextContainerBuilder

--- llamaindex_text_to_sql_100_knocks/main.py
+++ llamaindex_text_to_sql_100_knocks/main.py
@@ -32,8 +32,22 @@ def main():
         table_name="receipt",
     )
 
-    response = index.query(
-        'レシート明細データ（receipt）と店舗データ（store）を内部結合し、レシート明細データの全項目と店舗データの店舗名（store_name）を10件表示せよ。')
+    context_builder = SQLContextContainerBuilder(sql_database)
+    table_schema_index = context_builder.derive_index_from_context(
+        GPTSimpleVectorIndex,
+        # store_index=True
+    )
+
+    query_str = 'レシート明細データ（receipt）と店舗データ（store）を内部結合し、レシート明細データの全項目と店舗データの店舗名（store_name）を10件表示せよ。'
+
+    context_builder.query_index_for_context(
+        table_schema_index,
+        query_str,
+        store_context_str=True
+    )
+    context_container = context_builder.build_context_container()
+
+    response = index.query(query_str, sql_context_container=context_container)
     print(response.extra_info['sql_query'])
     print(response)
 
