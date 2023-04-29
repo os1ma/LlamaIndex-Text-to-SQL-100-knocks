6ca8bc6518cb67a1f4ac10dbb75ea845615fca26
Yuki Oshima <39944763+os1ma@users.noreply.github.com>
2023-04-29 13:06:40 +0900

    Delete SQLContextContainerBuilder

--- llamaindex_text_to_sql_100_knocks/main.py
+++ llamaindex_text_to_sql_100_knocks/main.py
@@ -1,6 +1,5 @@
 from sqlalchemy import create_engine
-from llama_index import GPTSQLStructStoreIndex, SQLDatabase, GPTSimpleVectorIndex
-from llama_index.indices.struct_store import SQLContextContainerBuilder
+from llama_index import GPTSQLStructStoreIndex, SQLDatabase
 
 import logging
 import sys
@@ -25,29 +24,15 @@ def main():
         **pgconfig)
     engine = create_engine(database_url)
 
-    sql_database = SQLDatabase(engine, include_tables=["receipt"])
+    sql_database = SQLDatabase(engine)
     index = GPTSQLStructStoreIndex(
         [],
         sql_database=sql_database,
-        table_name="receipt",
-    )
-
-    context_builder = SQLContextContainerBuilder(sql_database)
-    table_schema_index = context_builder.derive_index_from_context(
-        GPTSimpleVectorIndex,
-        # store_index=True
     )
 
     query_str = 'レシート明細データ（receipt）と店舗データ（store）を内部結合し、レシート明細データの全項目と店舗データの店舗名（store_name）を10件表示せよ。'
 
-    context_builder.query_index_for_context(
-        table_schema_index,
-        query_str,
-        store_context_str=True
-    )
-    context_container = context_builder.build_context_container()
-
-    response = index.query(query_str, sql_context_container=context_container)
+    response = index.query(query_str)
     print(response.extra_info['sql_query'])
     print(response)
 
